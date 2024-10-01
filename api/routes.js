a1ddbe8996a9d68c8940889f0b45046cb9f8a928
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-10-02 01:15:39 +0530

    feat: google auth

--- api/routes.js
+++ api/routes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const { Service } = require("../services");
-const { BadRequestError, UnauthorizedError } = require("../utils/errors");
+const { BadRequestError, UnauthorizedError, InternalServerError } = require("../utils/errors");
 const passport = require("passport");
 const authMiddleware = require('../middlewares/auth');
 const validateMiddleware = require('../middlewares/validate');
-const { UserSchema,StudentSchema } = require('../schemas')
-const{s3,upload,uploadFileToS3}= require("../config/awsconfig")
+const { UserSchema, StudentSchema } = require('../schemas')
+const { s3, upload, uploadFileToS3 } = require("../config/awsconfig")
 const { oauth2Client } = require('../config/googleconfig');
+const axios = require('axios');
 
 const router = express.Router();
 const service = new Service();
 const userSchema = new UserSchema();
-const studentSchema=new StudentSchema();
+const studentSchema = new StudentSchema();
 
 router.get("/", (req, res) => {
   res.json({ message: "Welcome to the users API" });
@@ -39,48 +40,30 @@ router.get("/allusers", authMiddleware, async (req, res) => {
   return res.json(data);
 });
 
-router.get('/google', async (req, res, next) => {
+router.post('/google', async (req, res) => {
   const code = req.body.code;
 
-      const googleRes = await oauth2Client.getToken(code);
-      oauth2Client.setCredentials(googleRes.tokens);
-      // const userRes = await axios.get(
-      //     `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
-      // );
 
-      const userRes = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-        });
+  const googleRes = await oauth2Client.getToken(code);
 
-        
+  if (!googleRes)
+    throw new BadRequestError("Unable to fetch token");
+
+  oauth2Client.setCredentials(googleRes.tokens);
+  const userRes = await axios.get(
+    `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
+  );
 
 
-      
-      const { email } = userRes.data;
-      // console.log(userRes);
-      let user = await User.findOne({ email });
+  if (!userRes)
+    throw new UnauthorizedError("Unable to fetch google profile")
 
-      if (!user) {
-          user = await User.create({
-              name,
-              email,
-              image: picture,
-          });
-      }
-      const { _id } = user;
-      const token = jwt.sign({ _id, email },
-          process.env.JWT_SECRET, {
-          expiresIn: process.env.JWT_TIMEOUT,
-      });
-      res.status(200).json({
-          message: 'success',
-          token,
-          user,
-      });
-  
+
+  const data = await service.googleAuth(userRes.data.id, userRes.data.email);
+
+  res.status(200).json(data);
+
+
 });
 
 // router.get('/login/google/callback', passport.authenticate('google', {
@@ -118,34 +101,34 @@ router.get('/google', async (req, res, next) => {
 
 //student profile routes
 
-router.post("/StudentProfile",authMiddleware, validateMiddleware(studentSchema.profileSchema), async (req, res) => {
-  const { firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity} = req.body;
+router.post("/StudentProfile", authMiddleware, validateMiddleware(studentSchema.profileSchema), async (req, res) => {
+  const { firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity } = req.body;
 
-  const userId=req.userId;
+  const userId = req.userId;
   const data = await service.createStudentProfile(firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity, userId);
   return res.status(201).json(data);
 
 });
 
-router.put("/StudentProfile",authMiddleware, async (req, res) => {
-  const { firstName,lastName,contactNumber,gender,city,country,skills,preparingFor,workMode,preferredCity,resumeLink } = req.body;
-  
-  const userId=req.userId;
-  const data = await service.updateStudentProfile(firstName,lastName,contactNumber,gender,city,country,skills,preparingFor,workMode,preferredCity,resumeLink,userId);
+router.put("/StudentProfile", authMiddleware, async (req, res) => {
+  const { firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity, resumeLink } = req.body;
+
+  const userId = req.userId;
+  const data = await service.updateStudentProfile(firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity, resumeLink, userId);
   return res.status(204).json(data);
 
 });
 
-router.get("/StudentProfile",authMiddleware, async (req, res) => {
-  const userId=req.userId;
+router.get("/StudentProfile", authMiddleware, async (req, res) => {
+  const userId = req.userId;
   const data = await service.getStudentProfile(userId);
   return res.status(200).json(data);
 });
 
 
 
-router.post("/studentprofilewithresume",authMiddleware,upload.single('file'),validateMiddleware(studentSchema.profileSchema),async (req, res) => {
-  const { firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity} = req.body;
+router.post("/studentprofilewithresume", authMiddleware, upload.single('file'), validateMiddleware(studentSchema.profileSchema), async (req, res) => {
+  const { firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity } = req.body;
 
   // console.log(req.body);
 
@@ -158,8 +141,8 @@ router.post("/studentprofilewithresume",authMiddleware,upload.single('file'),val
   const resumeLink = await uploadFileToS3(filePath, fileName);
   // console.log(resumeLink);
 
-  const userId=req.userId;
-  const data = await service.createStudentProfilewithresume(firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity,resumeLink, userId);
+  const userId = req.userId;
+  const data = await service.createStudentProfilewithresume(firstName, lastName, contactNumber, gender, city, country, skills, preparingFor, workMode, preferredCity, resumeLink, userId);
   return res.status(201).json(data);
 
 });
@@ -171,7 +154,7 @@ router.post("/studentprofilewithresume",authMiddleware,upload.single('file'),val
 
 
 // Upload resume
-router.post('/uploadresume',authMiddleware, upload.single('file'), async (req, res) => {
+router.post('/uploadresume', authMiddleware, upload.single('file'), async (req, res) => {
   try {
     const filePath = req.file.path;
     const fileName = req.file.filename;
@@ -183,19 +166,19 @@ router.post('/uploadresume',authMiddleware, upload.single('file'), async (req, r
     // console.log(fileUrl);
 
     res.status(200).json({
-        message: 'Resume uploaded successfully',
-        resumeUrl: fileUrl,
+      message: 'Resume uploaded successfully',
+      resumeUrl: fileUrl,
     });
-} catch (error) {
+  } catch (error) {
     res.status(500).json({ error: error.message });
-}
+  }
 });
 
 //Update resume
-router.post('/updateresume',authMiddleware, upload.single('file'),async (req, res) => {
+router.post('/updateresume', authMiddleware, upload.single('file'), async (req, res) => {
   try {
     // const { username} = req.body;
-    const userid=req.userId
+    const userid = req.userId
     const oldFileName = `${userid}.${req.file.originalname.split('.').pop()}`;
 
     const filePath = req.file.path;
@@ -206,28 +189,28 @@ router.post('/updateresume',authMiddleware, upload.single('file'),async (req, re
 
     // Delete old file from S3 before uploading the new one
     s3.deleteObject({
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: oldFile,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: oldFile,
     }, async (err) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        // Upload new file to S3
-        const fileUrl = await uploadFileToS3(filePath, newFileName);
-        res.status(201).json({
-            message: 'Resume updated successfully',
-            resumeUrl: fileUrl,
-        });
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      // Upload new file to S3
+      const fileUrl = await uploadFileToS3(filePath, newFileName);
+      res.status(201).json({
+        message: 'Resume updated successfully',
+        resumeUrl: fileUrl,
+      });
     });
-} catch (error) {
+  } catch (error) {
     res.status(500).json({ error: error.message });
-}
+  }
 });
 
 // Delete resume
-router.delete('/deleteresume',authMiddleware, async (req, res) => {
+router.delete('/deleteresume', authMiddleware, async (req, res) => {
   try {
-    const userid=req.userId
+    const userid = req.userId
     // const fileName = `${userid}.${req.file.originalname.split('.').pop()}`;
     const fileName = `${userid}.pdf`;
 
@@ -235,23 +218,23 @@ router.delete('/deleteresume',authMiddleware, async (req, res) => {
 
     // Delete the file from S3
     s3.deleteObject({
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: fileName,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: fileName,
     }, (err) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        //else {
-        //   //Remove the file from the local uploads folder
-        //   fs.unlinkSync(filePath);
-        //   resolve(data.Location);
-        // }
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      //else {
+      //   //Remove the file from the local uploads folder
+      //   fs.unlinkSync(filePath);
+      //   resolve(data.Location);
+      // }
 
-        res.status(200).json({ message: 'Resume deleted successfully' });
+      res.status(200).json({ message: 'Resume deleted successfully' });
     });
-} catch (error) {
+  } catch (error) {
     res.status(500).json({ error: error.message });
-}
+  }
 });
 
 module.exports = router;
