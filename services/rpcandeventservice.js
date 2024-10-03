b900eacaf34fd3e8291e7ed63aa3410480d82c3d
unknown <prathameshshriramwar100@gmail.com>
2024-10-03 22:18:16 +0530

    presigned url

--- services/rpcandeventservice.js
+++ services/rpcandeventservice.js
@@ -43,3 +43,83 @@ class UserService {
   }
 }
 module.exports = {UserService};
+
+
+
+
+
+
+
+
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+} = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+// const crypto = require("crypto");
+
+const {
+  AWS_S3_BUCKET_NAME,
+  AWS_REGION,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  SIGNED_URL_EXPIRATION,
+} = require("../config");
+
+const s3 = new S3Client({
+  region: BUCKET_REGION,
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  },
+});
+
+const addImage = async (file) => {
+  const randomString = () => crypto.randomBytes(32).toString("hex");
+  const fileName = randomString();
+
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+  };
+
+  try {
+    await s3.send(new PutObjectCommand(params));
+  } catch (error) {
+    console.log("Error", error);
+  }
+
+  return fileName;
+};
+
+const deleteImage = async (fileName) => {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName,
+  };
+
+  try {
+    await s3.send(new DeleteObjectCommand(params));
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
+const getSignedUrlForRead = async (fileName) => {
+  const command = new GetObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: fileName,
+  });
+
+  const signedUrl = await getSignedUrl(s3, command, {
+    expiresIn: SIGNED_URL_EXPIRATION,
+  });
+
+  return signedUrl;
+};
+
+module.exports = { addImage, deleteImage, getSignedUrlForRead };
