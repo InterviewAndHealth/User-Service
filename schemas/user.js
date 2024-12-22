06edf87ac4ccc457ec9cfcc68576ff09e4cc3adc
unknown <prathameshshriramwar100@gmail.com>
2024-12-22 14:51:19 +0530

    bug fix

--- schemas/user.js
+++ schemas/user.js
@@ -9,7 +9,7 @@ class UserSchema {
   registerSchema = joi.object().keys({
     email: joi.string().email().required(),
     password: joi.string().min(5).required(),
-    referral_code: joi.string().optional(),
+    referral_code: joi.string().optional().allow(null),
     // name: joi.string().min(2).required()
   });
 
